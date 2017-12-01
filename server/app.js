10875d4985c206a6a3f7914dbe13bf15947a5e68
Ryan Spencer <rxs2833@g.rit.edu>
2017-12-01 17:04:32 -0500

    guess i just lose a bunch of points cool

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.envMONGODB_URI || 'mongodb://localhost/gatcha';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/gatcha';
 
 mongoose.connect(dbURL, { useMongoClient: true }, (err) => {
   if (err) {
